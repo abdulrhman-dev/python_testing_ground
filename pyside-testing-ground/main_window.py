2f5d0dd0bfb6aed824fb5d1c21a4213ccf7ecb14
abdulrhman-dev <abdulrhman.jalal.dev@gmail.com>
2023-07-21 16:26:20 +0300

    text-editor

--- pyside-testing-ground/main_window.py
+++ pyside-testing-ground/main_window.py
@@ -1,13 +1,16 @@
-from PySide6.QtWidgets import QMainWindow, QLabel, QPushButton, QLineEdit, QVBoxLayout, QHBoxLayout, QWidget
-from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QTextEdit, QVBoxLayout, QHBoxLayout
+from PySide6.QtCore import Slot, QSize
+from PySide6.QtGui import QIcon
 
 
 class MainWindow(QMainWindow):
     def __init__(self, app):
         super().__init__()
-        self.setWindowTitle("Line Edit Test")
+        self.setWindowTitle("Wierd Text Editor")
+        self.setMinimumSize(QSize(700, 500))
 
         main_widget = MainWidget()
+
         self.setCentralWidget(main_widget)
 
 
@@ -16,36 +19,50 @@ class MainWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-        line_edit_label = QLabel("Enter something: ")
+        copy_button = QPushButton(QIcon('./icons/clipboard.svg'), "Copy")
+        cut_button = QPushButton(QIcon('./icons/scissors.svg'), "Cut")
+        paste_button = QPushButton(
+            QIcon('./icons/clipboard2-fill.svg'), "Paste")
+        undo_button = QPushButton(
+            QIcon('./icons/arrow-counterclockwise.svg'), "Undo")
+        redo_button = QPushButton(
+            QIcon('./icons/arrow-clockwise.svg'), "Redo")
+        plain_text_button = QPushButton(
+            QIcon('./icons/file-text.svg'), "Set Plain Text")
+        html_button = QPushButton(
+            QIcon('./icons/filetype-html.svg'), "Set Html")
+        clear_button = QPushButton(QIcon('./icons/trash3.svg'), "Clear")
 
-        self.line_edit = QLineEdit()
-        self.line_edit.textChanged.connect(self.handle_text_change)
-        self.line_edit.textEdited.connect(self.handle_text_edit)
+        self.text_edit = QTextEdit()
 
-        confirm_button = QPushButton("Do Amazing things")
-        confirm_button.clicked.connect(self.change_text)
+        # connections
+        copy_button.clicked.connect(self.text_edit.copy)
+        cut_button.clicked.connect(self.text_edit.cut)
+        paste_button.clicked.connect(self.text_edit.paste)
+        undo_button.clicked.connect(self.text_edit.undo)
+        redo_button.clicked.connect(self.text_edit.redo)
+        html_button.clicked.connect(self.button_to_html)
+        plain_text_button.clicked.connect(self.button_plain_text)
+        clear_button.clicked.connect(self.text_edit.clear)
 
-        self.output_label = QLabel("I'm nothing but a mere placeholder")
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(copy_button)
+        button_layout.addWidget(cut_button)
+        button_layout.addWidget(paste_button)
+        button_layout.addWidget(undo_button)
+        button_layout.addWidget(redo_button)
+        button_layout.addWidget(plain_text_button)
+        button_layout.addWidget(html_button)
+        button_layout.addWidget(clear_button)
 
         main_layout = QVBoxLayout()
-        input_layout = QHBoxLayout()
-
-        input_layout.addWidget(line_edit_label)
-        input_layout.addWidget(self.line_edit)
-        main_layout.addLayout(input_layout)
-        main_layout.addWidget(confirm_button)
-        main_layout.addWidget(self.output_label)
+        main_layout.addLayout(button_layout)
+        main_layout.addWidget(self.text_edit)
 
         self.setLayout(main_layout)
 
-    @Slot()
-    def change_text(self):
-        self.output_label.setText(self.line_edit.text())
+    def button_plain_text(self):
+        self.text_edit.setPlainText(self.text_edit.toPlainText())
 
-    # @Slot()
-    # def handle_text_change(self, value):
-    #     print(value)
-
-    # @Slot()
-    # def handle_text_edit(self, value):
-    #     print(value)
+    def button_to_html(self):
+        self.text_edit.setText(self.text_edit.toPlainText())
