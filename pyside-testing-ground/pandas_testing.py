2d94033bce792e85878e8d89ab4c336939b1ddf3
abdulrhman-dev <abdulrhman.jalal.dev@gmail.com>
2023-07-23 16:23:37 +0300

    tabel test

---
+++ pyside-testing-ground/pandas_testing.py
@@ -0,0 +1,16 @@
+# import pandas as pd
+
+# df = pd.read_excel("./file_example_XLSX_5000.xlsx")
+
+# column_number, row_number = df.shape
+# columns = list(df.columns)
+# print(columns)
+
+# for row in df.to_dict('records'):
+#     print(row)
+
+
+random_list = ["hello", "world", "there", "is", "wierd", "things"]
+
+for index, item in enumerate(random_list):
+    print(item)
